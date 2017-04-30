8682305bbcc1e65665dc2a1b809eb983ab8c7d1e
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-05-01 01:10:23 +0530

    added userHome page

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ socket io for integrating pub sub model for real time tracking of mongodb databa
 chalk used for simple styling of console outputs.
 ejs used as the templating engine
 */
-var express=require('express');
-var io=require('socket.io');
-var chalk=require('chalk');
+var express=require('express')
+var io=require('socket.io')
+var chalk=require('chalk')
 
-var app=express();
-var server=app.listen(4000);
+var app=express()
+var server=app.listen(4000)
 
 // socket io also listening to that same server
-io.listen(server); 
+io.listen(server);
 
 //setting the view engine, in this case embedded javascript or ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 //just logging that the server has started
 console.log(chalk.blue('Server started!!!'))
@@ -23,3 +23,11 @@ console.log(chalk.blue('Server started!!!'))
 app.get('/', function(req, res){
     res.render('index')
 });
+
+app.get('/admin', function(req, res){
+    res.render('admin')
+});
+
+app.get('/userHome', function (req, res){
+    res.render('userHome')
+})
