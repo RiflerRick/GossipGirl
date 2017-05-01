f95481d983bb5d2a142a46ae1512142977875690
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-05-02 00:24:52 +0530

    updated findQuery for findAPIs for database

--- DB/DbOps.js
+++ DB/DbOps.js
@@ -2,8 +2,11 @@ var path=require('path')
 var jsonfile=require('jsonfile')
 var file='config.json'
 var chalk=require('chalk')
-var GSDocStructure=require('gsDocStructure.json')
-var userDocStructure=require('userDocStructure.json')
+var GSDocStructureFile='gsDocStructure.json'
+var userDocStructureFile='userDocStructure.json'
+
+var GSDocStructure=json.readFileSync(GSDocStructureFile)
+var userDocStructure=jsonfile.readFileSync(userDocStructureFile)
 
 /*
 reading of the config json file has to be a synchronous operation as the contents of the file are crucial to the correct working of the rest of the application
@@ -74,18 +77,59 @@ exports.insert={
         //here when we are inserting a user we assume that the user does not exist before hand
 
         //creating the document from layout given in userDocStructure
+        userDocStructure.email=email
 
         database.collection(userCollection,function(err, collection){
-            collection.insert(data)
+            if(err)
+            {
+                return console.log(console.red('error during collection instantiation'))
+            }
+            collection.insert(userDocStructure)
             console.log(chalk.green("user successfully written to database"))
         })
     
     },
     insertGSData:function insertData(data){
         //log for maintaining gossip girl character records
+
+        //data here is a javascript object that simply stores the character related data
+
+        GSDocStructure.character_name=data.character_name
+        GSDocStructure.location=data.location
+        GSDocStructure.relationships=data.relationships
+        GSDocStructure.job=data.job
+        GSDocStructure.assignment=data.assignment
+
         database.collection(GSCollection,function(err, collection){
-            collection.insert(data)
-            console.log(chalk.green("user successfully written to database"))
+            if (err)
+            {
+                console.log(chalk.red('error during collection instantiation'))
+            }
+            collection.insert(GSDocStructure)
+            console.log(chalk.green("log successfully made"))
+        })
+    },
+    insertCharacterSubscriptions: function insertData(email, data){
+        //here we will append the subscriptions of a user.
+
+        userDocStructure.email=email
+
+        GSDocStructure.character_name=data.character_name
+        GSDocStructure.location=data.location
+        GSDocStructure.relationships=data.relationships
+        GSDocStructure.job=data.job
+        GSDocStructure.assignment=data.assignment
+
+        var updateQuery='{$push: { "characters":'+ GSDocStructure +'}}'
+
+        database.collection(userCollection, function(err, collection){
+            if (err)
+            {
+                console.log(chalk.red('error during collection instantiation'))
+            }
+            email='{ "email":'+email+'}'
+            collection.update(email, updateQuery)
+            console.log(chalk.green('successfully updated the user'))
         })
     }
 
@@ -94,8 +138,29 @@ exports.insert={
 exports.read={
     userExists:function readData(email){
         //returns if the user of that email exists
+        var findQuery='{"email":'+ email +'}'
+        database.collection(userCollection,function(err, collection){
+            if(err)
+            {
+                 console.log(chalk.red('error during collection instantiation'))
+            }
+            if(collection.find(findQuery).count()==1)
+                return 1
+            else
+                return 0
+        })
     },
-    userSubsciption: function readData(email){
-        //returns the fields that the user has subscribed to
+    checkUserSubscription: function readData(data){
+        //return the user email that have subscribed to this data
+
+        database.collection(userCollection, function(err, collection){
+            if(err)
+            {
+                console.log(chalk.red('error during collection instantiation'))   
+            }
+            var findQuery='{characters: { $elemMatch:{ character_name: { $or: [ {'+ data.character_name +'},{"NULL"}]}, location: { $or: [{'+ data.location+'}, {"NULL"}]}, relationships :{ $or:[{'+data.relationships+'},{"NULL"}]}, job:{$or:[{'+ data.job +'}, {"NULL"}]}, assignment: {$or:[{'+ data.assignment +'}, {"NULL"}]} }}}'
+
+            return collection.find(findQuery, '{ "email": 1 , "_id": 0}')
+        })
     }
 }
